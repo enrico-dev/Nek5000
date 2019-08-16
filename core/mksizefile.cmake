cfc7256a64dfac4d2f133ecf14cdc77ed96f7c55
Ronald Rahaman <rahaman@mcs.anl.gov>
2019-08-16 14:55:32 -0500

    WIP: Working mksizefile.cmake

--- core/mksizefile.cmake
+++ core/mksizefile.cmake
@@ -2,7 +2,8 @@
 # Read SIZE file
 # =============================================================================
 
-file(READ ${INFILE_DIR}/SIZE size_str)
+file(READ SIZE size_str)
+file(WRITE SIZE.bk "${size_str}")
 string(TOLOWER "${size_str}" size_str_lower)
 
 # =============================================================================
@@ -46,7 +47,7 @@ c automatically added by cmake
   if(NOT size_str_lower MATCHES "lxo")
     string(CONCAT size_str "${size_str}"
 "
-c automatically added by makenek
+c automatically added by cmake
       parameter(lxo   = lx1) ! max output grid size (lxo>=lx1)
 ")
   endif()
@@ -54,27 +55,25 @@ c automatically added by makenek
   if(NOT size_str_lower MATCHES "ax1")
     string(CONCAT size_str "${size_str}"
 "
-c automatically added by makenek
+c automatically added by cmake
       integer ax1,ay1,az1,ax2,ay2,az2
       parameter (ax1=lx1,ay1=ly1,az1=lz1,ax2=lx2,ay2=ly2,az2=lz2) ! running averages
 ")
   endif()
 
-# TODO: Implement this
-# if ! cat SIZE | grep -qi 'lys=lxs' ; then
-#   cat SIZE | grep -iv lxs > SIZE.x; mv SIZE.x SIZE  # Clean existing SIZE file of old version
-#   echo >>SIZE
-#   echo 'c automatically added by makenek' >>SIZE
-#   echo '      parameter (lxs=1,lys=lxs,lzs=(lxs-1)*(ldim-2)+1) !New Pressure Preconditioner' >> SIZE
-# fi
   if(NOT size_str_lower MATCHES "lys=lxs")
-    message(FATAL_ERROR "SIZE file must define: 'parameter (lxs=1,lys=lxs,lzs=(lxs-1)*(ldim-2)+1)'. Please make this change manually.")
+    string(REGEX REPLACE "[\r\n]+[^\r\n]+lxs[^\r\n]+[\r\n]+" "" size_str "${size_str}")
+    string(CONCAT size_str "${size_str}"
+"
+c automatically added by cmake
+      parameter (lxs=1,lys=lxs,lzs=(lxs-1)*(ldim-2)+1) !New Pressure Preconditioner
+")
   endif()
 
   if(NOT size_str_lower MATCHES "lcvx1")
     string(CONCAT size_str "${size_str}"
 "
-c automatically added by makenek
+c automatically added by cmake
       integer lcvx1,lcvy1,lcvz1,lcvelt
       parameter (lcvx1=1,lcvy1=1,lcvz1=1,lcvelt=1) ! cvode arrays
 ")
@@ -83,7 +82,7 @@ c automatically added by makenek
   if(NOT size_str_lower MATCHES "lfdm")
     string(CONCAT size_str "${size_str}"
 "
-c automatically added by makenek
+c automatically added by cmake
       parameter (lfdm=0)  ! == 1 for fast diagonalization method
 ")
   endif()
@@ -91,7 +90,7 @@ c automatically added by makenek
   if(NOT size_str_lower MATCHES "nsessmax")
     string(CONCAT size_str "${size_str}"
 "
-c automatically added by makenek
+c automatically added by cmake
       parameter (nsessmax=1)  ! max sessions to NEKNEK
 ")
   endif()
@@ -99,7 +98,7 @@ c automatically added by makenek
   if(NOT size_str_lower MATCHES "nmaxl_nn")
     string(CONCAT size_str "${size_str}"
 "
-c automatically added by makenek
+c automatically added by cmake
       parameter (nmaxl_nn=
      $          min(1+(nsessmax-1)*2*ldim*lxz*lelt,2*ldim*lxz*lelt))
 ")
@@ -108,27 +107,29 @@ c automatically added by makenek
   if(NOT size_str_lower MATCHES "nfldmax_nn")
     string(CONCAT size_str "${size_str}"
 "
-c automatically added by makenek' >>SIZE
-      parameter (nfldmax_nn=' >> SIZE
-     $          min(1+(nsessmax-1)*(ldim+1+ldimt),ldim+1+ldimt))' >>SIZE
+c automatically added by cmake
+      parameter (nfldmax_nn=
+     $          min(1+(nsessmax-1)*(ldim+1+ldimt),ldim+1+ldimt))
 ")
   endif()
 
   if(NOT size_str_lower MATCHES "nio")
     string(CONCAT size_str "${size_str}"
 "
-c automatically added by makenek' >>SIZE
-      common/IOFLAG/nio  ! for logfile verbosity control' >> SIZE
+c automatically added by cmake
+      common/IOFLAG/nio  ! for logfile verbosity control
 ")
   endif()
 
   if(NOT size_str_lower MATCHES "toteq")
     string(CONCAT size_str "${size_str}"
 "
-c automatically added by makenek
+c automatically added by cmake
       integer toteq
       parameter(toteq = 5  ) ! Number of conserved variables
 ")
   endif()
 
 endif()
+
+file(WRITE SIZE "${size_str}")
